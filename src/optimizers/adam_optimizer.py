926d57ba4916686c546684fa41383d88f2f3e80a
enrique-dealba <140011964+enrique-dealba@users.noreply.github.com>
2024-10-04 13:49:24 -0500

    feature: AdamW

--- src/optimizers/adam_optimizer.py
+++ src/optimizers/adam_optimizer.py
@@ -2,4 +2,4 @@ import torch.optim as optim
 
 
 def get_adam_optimizer(params, learning_rate, weight_decay):
-    return optim.Adam(params, lr=learning_rate, weight_decay=weight_decay, eps=1e-5)
+    return optim.AdamW(params, lr=learning_rate, weight_decay=weight_decay, eps=1e-5)
