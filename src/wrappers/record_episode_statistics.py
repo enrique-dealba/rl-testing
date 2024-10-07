900165f19c2091db290a156da00d71c9aa7a7dcf
enrique-dealba <140011964+enrique-dealba@users.noreply.github.com>
2024-10-07 17:16:59 -0500

    debug: rewards

--- src/wrappers/record_episode_statistics.py
+++ src/wrappers/record_episode_statistics.py
@@ -19,15 +19,20 @@ class RecordEpisodeStatistics(gym.Wrapper):
         return observations, info
 
     def step(self, action):
+        """
+        info (dict) keys:
+        'env_id', 'players', 'lives', 'reward', 'terminated', 'elapsed_step'
+        """
         observations, rewards, terminated, truncated, info = super().step(action)
         # self.episode_returns += rewards
-        print(f"RecordEpisodeStatistics -- info keys: {info.keys()}")
-        self.episode_returns += info["rewards"]
+        self.episode_returns += info["reward"]
         self.episode_lengths += 1
         self.returned_episode_returns[:] = self.episode_returns
         self.returned_episode_lengths[:] = self.episode_lengths
-        self.episode_returns *= 1 - terminated
-        self.episode_lengths *= 1 - terminated
+        # self.episode_returns *= 1 - terminated
+        # self.episode_lengths *= 1 - terminated
+        self.episode_returns *= 1 - info["terminated"]
+        self.episode_lengths *= 1 - info["terminated"]
         info["r"] = self.returned_episode_returns
         info["l"] = self.returned_episode_lengths
         return (observations, rewards, terminated, truncated, info)
